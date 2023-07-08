f9907c7a9a1bbcf61bbcb88fc02196da4892c234
Jasbir <jasbirh1996@gmail.com>
2023-07-09 02:34:24 +0530

    shaadiAssignment first commit

---
+++ app/src/main/java/com/jb/shaadiassignment/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.jb.shaadiassignment
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.jb.shaadiassignment.ui.theme.ShaadiAssignmentTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ShaadiAssignmentTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ShaadiAssignmentTheme {
+        Greeting("Android")
+    }
+}
+
---
+++ app/src/main/java/com/jb/shaadiassignment/ShaadiApplication.kt
@@ -0,0 +1,8 @@
+package com.jb.shaadiassignment
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ShaadiApplication : Application() {
+}
